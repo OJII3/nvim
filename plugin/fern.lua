785b1e4260d2ba379ece13d117d85c72b43943d7
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-03-25 15:52:40 +0900

    update

--- plugin/fern.lua
+++ plugin/fern.lua
@@ -1,6 +1,6 @@
 local keymap = vim.keymap
 
-keymap.set('n', '<C-n>', "<cmd>Fern . -drawer -toggle<CR>", { noremap = true, silent = true })
+keymap.set('n', '<Space>e', "<cmd>Fern . -drawer -toggle<CR>", { noremap = true, silent = true })
 
 -- show hidden files by default
 vim.cmd [[let g:fern#default_hidden = 1]]
