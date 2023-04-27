acb52e360aa0f29ac8cc061798e82ed14efb0855
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-04-27 16:46:36 +0900

    Update

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,7 +1,7 @@
 local keymap = vim.keymap
 
 -- builtin
-keymap.set('n','<F1>',':edit $MYVIMRC<CR>')
+keymap.set('n', '<F1>', ':edit $MYVIMRC<CR>')
 keymap.set('t', '<ESC>', '<C-\\><C-n>', { noremap = true, silent = true })
 keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h', { noremap = true, silent = true })
 -- fern
@@ -15,13 +15,13 @@ keymap.set('n', '<Space>r', '<cmd>Telescope live_grep<CR>', { noremap = true, si
 keymap.set('n', 's', '<Plug>Lightspeed_s', { noremap = true, silent = true })
 -- coc
 keymap.set('i', '<C-Space>', 'coc#refresh()', { expr = true, silent = true }) -- trigger completion
-keymap.set('i', '<C-e>', '<Plug>(coc-snippet-expand-jump)') -- trigger snippet
-keymap.set('n', 'rn', '<Plug>(coc-rename)', { silent = true }) -- rename
+keymap.set('i', '<C-e>', '<Plug>(coc-snippet-expand-jump)')                   -- trigger snippet
+keymap.set('n', 'rn', '<Plug>(coc-rename)', { silent = true })                -- rename
 -- skkeleton
 keymap.set('i', '<C-j>', '<Plug>(skkeleton-enable)', { noremap = false, silent = true })
 keymap.set('c', '<C-j>', '<Plug>(skkeleton-enable)', { noremap = false, silent = true })
 -- markdown-preview
 keymap.set('n', '<Space>m', '<Plug>MarkdownPreviewToggle', { noremap = false, silent = true })
 -- toggleterm
-keymap.set('n', '<Space>t', '<cmd>ToggleTerm<CR>', { noremap = true, silent = true })
+keymap.set('n', '<Space>t', '<cmd>ToggleTerm<CR>i', { noremap = true, silent = true })
 keymap.set('t', '<Space>t', '<C-\\><C-n><cmd>ToggleTerm<CR>', { noremap = true, silent = true })
